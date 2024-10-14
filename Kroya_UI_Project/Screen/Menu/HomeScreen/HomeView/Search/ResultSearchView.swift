147f15c618112cd4317d9eb9e75d673c97a21a74
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-10-14 13:54:43 +0700

    last update on SaleModal

---
+++ Kroya_UI_Project/Screen/Menu/HomeScreen/HomeView/Search/ResultSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultSearchView.swift
+//  Kroya_UI_Project
+//
+//  Created by PVH_003 on 14/10/24.
+//
+
+import SwiftUI
+
+struct ResultSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultSearchView()
+}
