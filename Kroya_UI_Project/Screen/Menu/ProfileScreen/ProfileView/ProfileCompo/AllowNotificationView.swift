248aba00dd88e0fbb3429a6de4c364f46fea4e9e
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-10-15 14:53:26 +0700

    Modal View on Language, notification

---
+++ Kroya_UI_Project/Screen/Menu/ProfileScreen/ProfileView/ProfileCompo/AllowNotificationView.swift
@@ -0,0 +1,57 @@
+//
+//  AllowNotificationView.swift
+//  Kroya_UI_Project
+//
+//  Created by PVH_003 on 15/10/24.
+//
+
+import SwiftUI
+
+struct AllowNotificationView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+       
+            VStack{
+                VStack{
+                    Group{
+                        Text("Do you want to turn")
+                        Text("on Notification?")
+                    }
+                    .font(.customfont(.semibold, fontSize: 24))
+                    Spacer()
+                    Image("PushNotification")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity,maxHeight: 253)
+                    Spacer()
+                }
+                VStack{
+                    CustomButton(title: "Allow Notification", action: {print("Button Tap!")}, backgroundColor: .yellow, frameWidth: 370)
+                    CustomButton(title: "Not Now", action: {print("Button Tap!")}, backgroundColor: .clear, textColor: .yellow)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Button(action: {
+                            dismiss()
+                        }) {
+                            Image(systemName: "arrow.left")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                                .foregroundStyle(.black)
+                        }
+                    }
+                }
+            }
+        
+    }
+}
+
+#Preview {
+    AllowNotificationView()
+}
---
+++ Kroya_UI_Project/Screen/Menu/ProfileScreen/ProfileView/ProfileCompo/ChangeLanguageView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeLanguageView.swift
+//  Kroya_UI_Project
+//
+//  Created by PVH_003 on 15/10/24.
+//
+
+import SwiftUI
+
+struct ChangeLanguageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeLanguageView()
+}
