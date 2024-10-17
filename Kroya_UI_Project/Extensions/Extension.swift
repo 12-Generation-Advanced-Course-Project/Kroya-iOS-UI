829e6bec2252f11bd0f197d53eebd67bfd3c116b
OunBonaliheng <ounbonaliheng@gmail.com>
2024-10-17 13:46:17 +0700

    .

--- Kroya_UI_Project/Extensions/Extension.swift
+++ Kroya_UI_Project/Extensions/Extension.swift
@@ -76,22 +76,5 @@ extension Color {
 }
 
 
-// Extension to round specific corners
-extension View {
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape( RoundedCorner(radius: radius, corners: corners) )
-    }
-}
-
-// Custom Shape to handle rounded corners
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
 
 
