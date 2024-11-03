0f3b29eee25cb541b78489ee4bad8f09f5982cff
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-11-03 16:35:39 +0700

    add header in postView

--- Kroya_UI_Project/Screen/Menu/PostViewScreen/PostView/FoodSaleTabView/FoodSaleView.swift
+++ Kroya_UI_Project/Screen/Menu/PostViewScreen/PostView/FoodSaleTabView/FoodSaleView.swift
@@ -31,8 +31,8 @@ struct FoodSaleView: View {
                                     )
                     ) {
                         FoodOnSaleViewCell(foodSale: foodSale)
-                            .frame(width: 362)
-                            .padding(.top, 8)
+                            .padding(.horizontal, 20)
+                            .padding(.bottom,8)
                     }
                 }
                 
@@ -48,14 +48,15 @@ struct FoodSaleView: View {
                                     )
                     ) {
                         RecipeViewCell(recipe: recipe)
-                            .frame(width: 362)
-                            .padding(.top, 8)
+                            .padding(.horizontal, 20)
+                            .padding(.bottom,8)
                     }
                 }
             }
             .environmentObject(foodOnSaleViewModel)
             .environmentObject(recipeViewModel)
         }
+        .padding(.top,8)
     }
 }
 
