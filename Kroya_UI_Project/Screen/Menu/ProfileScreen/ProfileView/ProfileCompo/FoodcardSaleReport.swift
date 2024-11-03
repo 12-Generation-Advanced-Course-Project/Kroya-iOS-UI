858a8544206e75a8c0fcf5bf3e27e39a405651e0
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-11-03 17:56:58 +0700

    .

--- Kroya_UI_Project/Screen/Menu/ProfileScreen/ProfileView/ProfileCompo/FoodcardSaleReport.swift
+++ Kroya_UI_Project/Screen/Menu/ProfileScreen/ProfileView/ProfileCompo/FoodcardSaleReport.swift
@@ -17,9 +17,8 @@ struct ItemFoodOrderCard: View {
     
     @Binding var item: FoodItem
     var showEllipsis: Bool = true // Default to true for other uses
-    @State private var showPopover = false
-    
-    
+    @State  var showPopover = false
+    @Binding  var show3dot :Bool 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             HStack(spacing: 12) {
@@ -41,10 +40,11 @@ struct ItemFoodOrderCard: View {
                             RoundedRectangle(cornerRadius: 8)
                                 .fill(item.status == "Accept" ? Color(hex: "#DDF6C3") : (item.status == "Reject" ? Color(hex: "#FFD8E4") : Color.clear))
                                 .frame(width: 50, height: 23)
-                            
-                            Text(item.status != nil ? (item.status == "Accept" ? "Accept" : "Reject") : "")
-                                .font(.customfont(.regular, fontSize: 12))
-                                .foregroundColor(item.status == "Accept" ? .green : (item.status == "Reject" ? .red : .clear))
+                            if show3dot == true {
+                                Text(item.status != nil ? (item.status == "Accept" ? "Accept" : "Reject") : "")
+                                    .font(.customfont(.regular, fontSize: 12))
+                                    .foregroundColor(item.status == "Accept" ? .green : (item.status == "Reject" ? .red : .clear))
+                            }
                         }
                         
                         Spacer()
