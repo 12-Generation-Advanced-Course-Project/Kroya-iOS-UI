4f863e2bffa3863ad0bd77984f9393efba4f9ee2
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-11-06 17:12:10 +0700

    fixed

---
+++ Kroya_UI_Project/Screen/Menu/PostViewScreen/PostModel/ShowPostModel.swift
@@ -0,0 +1,12 @@
+//
+//  ShowPostModel.swift
+//  Kroya_UI_Project
+//
+//  Created by kosign on 6/11/24.
+//
+
+import Foundation
+
+struct ShowPostModel: Codable {
+    let message: String
+}
---
+++ Kroya_UI_Project/Screen/Menu/PostViewScreen/PostViewModel/ShowViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ShowViewModel.swift
+//  Kroya_UI_Project
+//
+//  Created by kosign on 6/11/24.
+//
+
