636a49ec354a15f49f24ec1846167ceb60862c17
ChhoySreynoch <chhoysreynoch@gmail.com>
2024-11-08 08:26:15 +0700

    add more url

--- Kroya_UI_Project/Utils/Constant.swift
+++ Kroya_UI_Project/Utils/Constant.swift
@@ -12,4 +12,6 @@ struct Constants {
     static let GoogleMapsAPIkeys = "AIzaSyCMUPCDeGjXm5kBNpFa-JosXCOur4SvlI8"
     static let KroyaAddress = "https://kroya-api-production.up.railway.app/api/v1/address/"
     static let fileupload = "https://kroya-api-production.up.railway.app/api/v1/fileView/"
+    static let CuisineUrl = "https://kroya-api-production.up.railway.app/api/v1/cuisine/"
+    static let CategoryUrl = "https://kroya-api-production.up.railway.app/api/v1/category/"
 }
