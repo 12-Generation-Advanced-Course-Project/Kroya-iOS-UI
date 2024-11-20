27062e017b4aac479a813e9d23c93ea7c2b4897b
pokhengly <henglypok21012021@gmail.com>
2024-11-20 09:20:03 +0700

    all

--- Kroya_UI_Project/Screen/Menu/OrderScreen/OrderView/OrderList/OrderListView.swift
+++ Kroya_UI_Project/Screen/Menu/OrderScreen/OrderView/OrderList/OrderListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct OrderListView: View {
 
+    var orderRequest: OrderRequestModel?
     var sellerId:Int
     @State private var show3dot:Bool = true
     @State private var searchText = ""
@@ -52,7 +53,7 @@ struct OrderListView: View {
                                 .frame(width: 16)
                                 .padding([.bottom, .leading], 12)
                                 .overlay(
-                                    Text("8")
+                                    Text("\(orderRequest?.quantity ?? 0)")
                                         .customFontSemiBoldLocalize(size: 10)
                                         .foregroundColor(.white)
                                         .padding([.bottom, .leading], 12)
